ed29567662f9ca359299fb0cd54ed5bfa899ba61
Chengkai Yang <yangchengkai@gmail.com>
2021-02-05 14:40:16 +0100

    Add some code so it's can read ssl and override it in code, for Azure App Service

--- Program.cs
+++ Program.cs
@@ -1,27 +1,54 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 
 namespace ChyaGrpc
 {
    public class Program
    {
+      static IConfiguration m_Config;
+
       public static void Main(string[] args)
       {
+         //m_Config = new ConfigurationBuilder()
+         //            .SetBasePath(Directory.GetCurrentDirectory())
+         //            .AddJsonFile("appsettings.json", optional: true)
+         //            .AddCommandLine(args)
+         //            .Build();
+
          CreateHostBuilder(args).Build().Run();
       }
 
-      // Additional configuration is required to successfully run gRPC on macOS.
-      // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
       public static IHostBuilder CreateHostBuilder(string[] args) =>
-          Host.CreateDefaultBuilder(args)
-              .ConfigureWebHostDefaults(webBuilder =>
-              {
-                 webBuilder.UseStartup<Startup>();
-              });
+         Host.CreateDefaultBuilder(args)
+             .ConfigureWebHostDefaults(webBuilder =>
+                                       {
+                                          webBuilder.UseStartup<Startup>();
+                                       });
+
+      //public static IHostBuilder CreateHostBuilder(string[] args) =>
+      //    Host.CreateDefaultBuilder(args)
+      //        .ConfigureWebHostDefaults(webBuilder =>
+      //        {
+      //           webBuilder.UseStartup<Startup>().ConfigureKestrel(ConfigKestrel);
+      //        });
+
+      private static void ConfigKestrel(KestrelServerOptions kestrelOpt)
+      {
+         //kestrelOpt.ListenAnyIP(5001, ConfigureListen);
+      }
+
+      static void ConfigureListen(ListenOptions listenOpt)
+      {
+         //var sslPath = m_Config.GetValue<string>("Kestrel:Certificates:Default:Path");
+         //var sslPwd = m_Config.GetValue<string>("Kestrel:Certificates:Default:Password");
+         //listenOpt.UseHttps(sslPath, sslPwd);
+      }
    }
 }
